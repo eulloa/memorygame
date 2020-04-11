bd425ba004c08888f1d6315b2285ae70f2716038
Efren Ulloa <eulloa345@gmail.com>
2020-04-10 21:25:15 -0500

    Updates

---
+++ precache-manifest.4d16cf3add3e9054b71ac100df008248.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f4a015e64942916751dcb69716b5f4f",
+    "url": "/memorygame/index.html"
+  },
+  {
+    "revision": "c6cc02c9542f98d429ec",
+    "url": "/memorygame/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6001591de400abfc2703",
+    "url": "/memorygame/static/js/2.da940bdc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/memorygame/static/js/2.da940bdc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6cc02c9542f98d429ec",
+    "url": "/memorygame/static/js/main.614dbbf1.chunk.js"
+  },
+  {
+    "revision": "63a41b52ac708ed1f737",
+    "url": "/memorygame/static/js/runtime-main.d2a7ef91.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/memorygame/static/media/logo.5d5d9eef.svg"
+  }
+]);
