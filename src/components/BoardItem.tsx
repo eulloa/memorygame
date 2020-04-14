1928e4d13b6d2b92771830a97b42e8dee6fec31a
Efren Ulloa <eulloa345@gmail.com>
2020-04-13 21:17:49 -0500

    feat: add card flip

--- src/components/BoardItem.tsx
+++ src/components/BoardItem.tsx
@@ -1,11 +1,12 @@
 import * as React from 'react';
+import blue from '../images/blue.png';
+import front from '../images/front.png';
+import red from '../images/red.png';
 
 type BoardItemProps = {
    found?: boolean | null;
    item: {
       background: string,
-      front: string,
-      type: string,
       value: string
    },
    onClick: (e: React.SyntheticEvent) => void;
@@ -14,15 +15,17 @@ type BoardItemProps = {
 
 export const BoardItem = React.memo(({ found, item, onClick, selected }: BoardItemProps) => (
    <div
-      className={`${item.value} board__item ${found ? 'found' : ''} ${selected ? 'selected': ''} bg-${item.background}`}
+      className={`${item.value} flip-container board__item ${found ? 'found' : ''} ${selected ? 'selected': ''}`}
       onClick={onClick}
    >
-      <section>
-         {item.front}
-      </section>
-      <section className={`${selected ? '' : 'hide'}`}>
-         {item.type}
-         {item.value}
-      </section>
+      <div className={`flipper ${selected ? 'hover': ''}`}>
+         <section className={`front flipper ${selected ? 'hover': ''}`}>
+            <img src={front} alt="front of playing card" />
+            <span>{item.value}</span>
+         </section>
+         <section className="back">
+            <img src={item.background === 'red' ? red : blue} alt="back of playing card" />
+         </section>
+      </div>
    </div>
 ));
--- src/data/BoardData.ts
+++ src/data/BoardData.ts
@@ -1,12 +1,12 @@
 export const BoardData = [
-   { value: 'rabbit', front: '9', type: 'clubs', found: false, background: 'red' },
-   { value: 'rabbit', front: '2', type: 'diamonds', found: false, background: 'blue' },
-   { value: 'cat', front: 'Q', type: 'hearts', found: false, background: 'blue' },
-   { value: 'cat', front: '3', type: 'clubs', found: false, background: 'blue' },
-   { value: 'dog', front: 'A', type: 'diamonds', found: false, background: 'red' },
-   { value: 'dog', front: '7', type: 'hearts', found: false, background: 'blue' },
-   { value: 'monkee', front: '7', type: 'spades', found: false, background: 'blue' },
-   { value: 'monkee', front: '6', type: 'hearts', found: false, background: 'blue' },
-   { value: 'whale', front: '7', type: 'spades', found: false ,background: 'red' },
-   { value: 'whale', front: 'J', type: 'clubs', found: false, background: 'blue' }
+   { value: 'rabbit', found: false, background: 'red' },
+   { value: 'rabbit', found: false, background: 'blue' },
+   { value: 'cat', found: false, background: 'blue' },
+   { value: 'cat', found: false, background: 'blue' },
+   { value: 'dog', found: false, background: 'red' },
+   { value: 'dog', found: false, background: 'blue' },
+   { value: 'monkee', found: false, background: 'blue' },
+   { value: 'monkee', found: false, background: 'blue' },
+   { value: 'whale', found: false ,background: 'red' },
+   { value: 'whale', found: false, background: 'blue' }
 ];
